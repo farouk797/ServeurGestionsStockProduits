c8257c9305108354fb20d3c0d0c86a7768eec8cd
farouk797 <you@example.com>
2019-08-14 13:34:07 +0200

    first commit

---
+++ src/main/java/com/example/demo/GestiondesProduitServeurJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiondesProduitServeurJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestiondesProduitServeurJavaApplication.class, args);
+	}
+
+}
